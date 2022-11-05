d7292eef588b222507d7fa83ca877e20b0a0e10b
jacobpatterson1549 <jacob.patterson@gmail.com>
2022-11-05 09:18:34 -0700

    moving image code to separate file

--- internal/server/server.go
+++ internal/server/server.go
@@ -4,14 +4,11 @@ package server
 import (
 	"compress/gzip"
 	"embed"
-	"encoding/base64"
 	"fmt"
 	"io"
 	"io/fs"
 	"net/http"
 	"net/url"
-	"os"
-	"os/exec"
 	"strconv"
 	"strings"
 	"text/template"
@@ -475,45 +472,4 @@ func prettyInputValue(i interface{}) interface{} {
 		return ""
 	}
 	return i
-}
-
-func parseImage(r *http.Request) (imageBase64 []byte, err error) {
-	f, fh, err := r.FormFile("image")
-	if err != nil {
-		if err == http.ErrMissingFile {
-			return nil, nil
-		}
-		return nil, err
-	}
-	b, err := io.ReadAll(f)
-	if err != nil {
-		return nil, fmt.Errorf("reading image: %w", err)
-	}
-	title := fh.Filename
-	b2, err := webP(b, title)
-	if err != nil {
-		return nil, fmt.Errorf("converting image to webp: %w", err)
-	}
-	imageBase64 = []byte(base64.StdEncoding.EncodeToString(b2))
-	return
-}
-
-// webP should be used in the kuuf-library server to encode uploaded jpg/png images
-func webP(b []byte, title string) ([]byte, error) {
-	// TODO: stream b to cwebp command.  As of 2022, this is not possible.
-	f, err := os.CreateTemp("", title)
-	if err != nil {
-		return nil, fmt.Errorf("creating temp file: %w", err)
-	}
-	n := f.Name()
-	if err := os.WriteFile(n, b, 0664); err != nil { // HACKY
-		return nil, fmt.Errorf("writing image to temporary file: %w", err)
-	}
-	defer os.Remove(n)
-	cmd := exec.Command("cwebp", n, "-o", "-")
-	b2, err := cmd.Output()
-	if err != nil {
-		return nil, fmt.Errorf("running cwebp: %w", err)
-	}
-	return b2, nil
 }
