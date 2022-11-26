f7f1c09839dadd20befae22ef828f57539846d2c
jacobpatterson1549 <jacob.patterson@gmail.com>
2022-11-26 09:35:10 -0800

    moving mongo-book logic into book.go
    leaving type definitions in database.go to make it clear that type tags and const field names should be the same

---
+++ internal/db/mongo/book.go
@@ -0,0 +1,58 @@
+package mongo
+
+import "github.com/jacobpatterson1549/kuuf-library/internal/book"
+
+func mongoBook(b book.Book) mBook {
+	return mBook{
+		Header:        mongoHeader(b.Header),
+		Description:   b.Description,
+		DeweyDecClass: b.DeweyDecClass,
+		Pages:         b.Pages,
+		Publisher:     b.Publisher,
+		PublishDate:   b.PublishDate,
+		AddedDate:     b.AddedDate,
+		EAN_ISBN13:    b.EAN_ISBN13,
+		UPC_ISBN10:    b.UPC_ISBN10,
+		ImageBase64:   b.ImageBase64,
+	}
+}
+
+func mongoHeader(h book.Header) mHeader {
+	return mHeader{
+		ID:      h.ID,
+		Title:   h.Title,
+		Author:  h.Author,
+		Subject: h.Subject,
+	}
+}
+
+func (m mBook) Book() book.Book {
+	return book.Book{
+		Header:        m.Header.Header(),
+		Description:   m.Description,
+		DeweyDecClass: m.DeweyDecClass,
+		Pages:         m.Pages,
+		Publisher:     m.Publisher,
+		PublishDate:   m.PublishDate,
+		AddedDate:     m.AddedDate,
+		EAN_ISBN13:    m.EAN_ISBN13,
+		UPC_ISBN10:    m.UPC_ISBN10,
+		ImageBase64:   m.ImageBase64,
+	}
+}
+
+func (m mHeader) Header() book.Header {
+	return book.Header{
+		ID:      m.ID,
+		Title:   m.Title,
+		Author:  m.Author,
+		Subject: m.Subject,
+	}
+}
+
+func (m mSubject) Subject() book.Subject {
+	return book.Subject{
+		Name:  m.Name,
+		Count: m.Count,
+	}
+}
